8d3f1506a29b0b06ad4fed019b5b1fd1c1337984
banana <lbsrex@gmail.com>
2022-10-09 21:22:07 +0530

    added initial setup for maven and springboot

---
+++ src/main/java/com/nihilistdbanana/ner_server/NerServerApplication.java
@@ -0,0 +1,13 @@
+package com.nihilistdbanana.ner_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NerServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NerServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nihilistdbanana/ner_server/NerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nihilistdbanana.ner_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NerServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
